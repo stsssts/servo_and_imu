8f201005683e4034e5bc3be55dcc4531a83bd297
stsssts <nagrizolich@gmail.com>
2018-08-10 17:30:23 +0300

    added script to make node running without root

---
+++ scripts/make_rootless.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+pushd ~/test_ws/devel/lib/servo_and_imu   # cd to the directory with your node
+sudo chown root:root servo_and_imu_node # change ownship to root
+sudo chmod a+rx servo_and_imu_node      # set as executable by all
+sudo chmod u+s servo_and_imu_node       # set the setuid bit
+popd
+
--- src/servo_and_imu_node.cpp
+++ src/servo_and_imu_node.cpp
@@ -13,32 +13,31 @@ extern "C"
 class PWMChannel
 {
 public:
-  PWMChannel(ros::NodeHandle& nh, int channel_id)
+  PWMChannel(ros::NodeHandle& nh, ros::NodeHandle& pnh, int channel_id)
+    : _channel_id(channel_id)
   {
-    _channel_id = channel_id;
-    std::string id = std::to_string(channel_id);
-
+    std::string topic;
     int min_pulse_width;
     int max_pulse_width;
     float min_scale_value;
     float max_scale_value;
-    std::string topic;
 
-    ros::NodeHandle pnh("~");
-    pnh.param("ch" + id + "_pulse_width", _pulse_width, int(1500));
-    pnh.param("ch" + id + "_min_pulse_width", min_pulse_width, int(1100));
-    pnh.param("ch" + id + "_max_pulse_width", max_pulse_width, int(1900));
-    pnh.param("ch" + id + "_min_value", min_scale_value, float(1100));
-    pnh.param("ch" + id + "_max_value", max_scale_value, float(1900));
-    pnh.param("ch" + id + "_topic", topic, "servo_" + id);
+    std::string id_str = std::to_string(channel_id);
+    pnh.param("ch" + id_str + "_pulse_width", _pulse_width, int(1500));
+    pnh.param("ch" + id_str + "_min_pulse_width", min_pulse_width, int(1100));
+    pnh.param("ch" + id_str + "_max_pulse_width", max_pulse_width, int(1900));
+    pnh.param("ch" + id_str + "_min_value", min_scale_value, float(1100));
+    pnh.param("ch" + id_str + "_max_value", max_scale_value, float(1900));
+    pnh.param("ch" + id_str + "_topic", topic, "servo_" + id_str);
 
     scale_k = (max_pulse_width - min_pulse_width) / (max_scale_value - min_scale_value);
     scale_shift = (max_scale_value * min_pulse_width - min_scale_value * max_pulse_width) /
         (max_scale_value - min_scale_value);
 
     _subscriber = nh.subscribe(topic + std::string("/pulse_width"), 10,
-                                &PWMChannel::callback, this);
-    ROS_INFO("Servo %i initialised", channel_id);
+                               &PWMChannel::callback, this);
+
+    ROS_INFO("Channel %i initialized", channel_id);
   }
 
   void update()
@@ -54,15 +53,15 @@ public:
     return int(data*scale_k + scale_shift);
   }
 
-  void callback(const std_msgs::Float32 msg)
+  void callback(const std_msgs::Float32ConstPtr& msg)
   {
-    _pulse_width_received = scaleLinear(msg.data);
+    _pulse_width_received = scaleLinear(msg->data);
   }
 
 private:
   ros::Subscriber _subscriber;
 
-  int _channel_id;
+  const int _channel_id;
   int _pulse_width;
   int _pulse_width_received;
 
@@ -70,106 +69,132 @@ private:
   float scale_shift;
 };
 
-class Imu
+class Servo
 {
 public:
-  Imu(ros::NodeHandle& nh) : _node("~")
+  Servo(ros::NodeHandle& nh, ros::NodeHandle& pnh)
+    : _initialized(false)
   {
-    // start with default config and modify based on options
-    rc_imu_config_t conf = rc_default_imu_config();
-
-    // now set up the imu for dmp interrupt operation
-    if(rc_initialize_imu_dmp(&_data, conf))
+    if(rc_enable_servo_power_rail())
     {
-      ROS_FATAL("rc_imu_initialize_failed");
+      ROS_ERROR("Failed to initialize servo power rail");
       return;
     }
 
-    ROS_INFO("Imu initialized");
-    _imu_publisher = nh.advertise<sensor_msgs::Imu>("imu", 1);
+    int frequency;
+    std::vector<int> ch;
+
+    pnh.param("frequency", frequency, 50);
+    pnh.param< std::vector<int> >("channels", ch, {1, 2, 3, 4, 5, 6, 7, 8});
+
+    for (auto ch_num : ch)
+      _channels.push_back(PWMChannel(nh, pnh, ch_num));
+
+    _initialized = true;
+    ROS_INFO("Servo initialization complete");
   }
 
-  void publishImu()
+  void update()
   {
-    sensor_msgs::Imu msg;
-    msg.header.frame_id = "robot";
-
-    msg.orientation.w = _data.dmp_quat[QUAT_W];
-    msg.orientation.x = _data.dmp_quat[QUAT_X];
-    msg.orientation.y = _data.dmp_quat[QUAT_Y];
-    msg.orientation.z = _data.dmp_quat[QUAT_Z];
-
-    msg.angular_velocity.x = _data.gyro[0];
-    msg.angular_velocity.y = _data.gyro[1];
-    msg.angular_velocity.z = _data.gyro[2];
-
-    _imu_publisher.publish(msg);
+    for (auto ch : _channels)
+      ch.update();
   }
+
+  bool ok() { return _initialized; }
+
+private:
+  std::vector<PWMChannel> _channels;
+  int _frequency;
+
+  bool _initialized;
+};
+
+class Imu
+{
+public:
+  Imu(ros::NodeHandle& nh, ros::NodeHandle& pnh)
+    : _initialized(false)
+  {
+    if(rc_initialize_imu_dmp(&_data, rc_default_imu_config()))
+    {
+      ROS_INFO("Failed to initialize IMU");
+      return;
+    }
+
+    _initialized = true;
+    _imu_publisher = nh.advertise<sensor_msgs::Imu>("imu", 1);
+    _msg.header.frame_id = "robot";
+
+    ROS_INFO("Imu initialized");
+  }
+
+  ~Imu()
+  {
+    if (_initialized)
+      rc_power_off_imu();
+  }
+
+  void update()
+  {
+    _msg.orientation.w = _data.dmp_quat[QUAT_W];
+    _msg.orientation.x = _data.dmp_quat[QUAT_X];
+    _msg.orientation.y = _data.dmp_quat[QUAT_Y];
+    _msg.orientation.z = _data.dmp_quat[QUAT_Z];
+
+    _msg.angular_velocity.x = _data.gyro[0];
+    _msg.angular_velocity.y = _data.gyro[1];
+    _msg.angular_velocity.z = _data.gyro[2];
+
+    _imu_publisher.publish(_msg);
+  }
+
+  bool ok() { return _initialized; }
 
 private:
   rc_imu_data_t _data;
-  ros::NodeHandle _node;
   ros::Publisher _imu_publisher;
+  sensor_msgs::Imu _msg;
+
+  bool _initialized;
 };
 
-/**
- * @brief      interrupt handler to catch ctrl-c
- */
 void signal_handler(__attribute__ ((unused)) int dummy)
 {
+  rc_set_state(EXITING);
+  rc_cleanup();
+  ROS_WARN("my signal handler");
   ros::shutdown();
-  return;
 }
 
 int main(int argc, char **argv)
 {
-  signal(SIGINT, signal_handler);
   ros::init(argc, argv, "servo_and_imu_node", ros::init_options::NoSigintHandler);
 
   ros::NodeHandle nh;
   ros::NodeHandle pnh("~");
 
-  int frequency, channels_len;
-  pnh.param("frequency", frequency, 50);
-  pnh.param("channels", channels_len, 8);
-
-  std::vector<PWMChannel> channels;
-
-  for (int i = 0; i < channels_len; ++i)
-    channels.push_back(PWMChannel(nh, i+1));
-
-  if(rc_initialize()<0)
+  if(rc_initialize())
   {
-    ROS_ERROR("Failed to initialize cape.");
+    ROS_ERROR("Failed to initialize cape");
     return EXIT_FAILURE;
   }
 
-  Imu imu(nh);
+  Imu imu(nh, pnh);
+  Servo servo(nh, pnh);
+  if (!imu.ok() || !servo.ok())
+    return EXIT_FAILURE;
 
-  rc_set_led(RED,1);
-  rc_set_led(GREEN,0);
-  rc_set_state(UNINITIALIZED);
-
-  if(rc_enable_servo_power_rail()){
-    fprintf(stderr,"failed to enable power rail\n");
-    return -1;
-  }
+  signal(SIGINT, signal_handler);
+  rc_set_state(RUNNING);
 
   ros::Rate loop_rate(10);
   while(ros::ok())
   {
-    for (auto ch : channels)
-      ch.update();
-    imu.publishImu();
-
+    imu.update();
+    servo.update();
     ros::spinOnce();
     loop_rate.sleep();
   }
-
-  rc_set_state(EXITING);
-  rc_power_off_imu();
-  rc_disable_servo_power_rail();
-  rc_cleanup();
 
   return EXIT_SUCCESS;
 }
